01035a83c9ff35b5e24b50df4e7e1944ff8758b6
Tash <psynb2@exmail.nottingham.ac.uk>
2020-10-17 14:54:25 +0100

    to add: reveal

--- minesweeper.c
+++ minesweeper.c
@@ -1,3 +1,6 @@
+// TO DO
+// in array store -2 for mines, -1 for hidden tiles, numbers for surrounding
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -5,29 +8,30 @@
 #define BOARD 9
 #define MINES 5
 
+#define HIDDEN -1
+#define MINE -2
+
 int createMines(int board[BOARD][BOARD]) {
-    for (int i=1; i<MINES; i++) {
+    for (int i=0; i<MINES; i++) {
         int randRow = rand() % BOARD + 1;
         int randCol = rand() % BOARD + 1;
-
-        printf("%d %d\n", randRow, randCol);
         
-        //while (board[randRow][randCol] = "X") {
-        //    randRow = rand() % (BOARD);
-        //    randCol = rand() % (BOARD);
-        //}
+        while (board[randRow][randCol] == 1) {
+            randRow = rand() % (BOARD);
+            randCol = rand() % (BOARD);
+        }
 
-        //printf("value %d\n", board[1][1]);
-
-
-        //int board[randRow][randCol] = "X";
+        board[randRow][randCol] = 1;
     }
-    printf("value %d\n", board[1][1]);
 
     return 0;
 }
 
-void RedrawBoard (int board[BOARD][BOARD]) {
+void reveal (int board [BOARD][BOARD], int row, int col) {
+
+}
+
+void ShowBoard (int board[BOARD][BOARD]) {
     system("clear");
 
     printf("  ");
@@ -41,7 +45,12 @@ void RedrawBoard (int board[BOARD][BOARD]) {
     for (int x=0; x<BOARD; x++) {
         printf("%d ", x+1);
         for (int y=0; y<BOARD; y++){
-            printf("%d ", board[x][y]);
+            if (board[x][y] == 0) {
+                printf(" ");
+            }
+            else if (board[x][y] == 1){
+                printf("X");
+            }
         }
         printf("\n");
     }
@@ -68,23 +77,13 @@ void PrintBoard() {
     printf("\n");
 }
 
-bool isMine(int row, int col, int board[BOARD][BOARD]) { 
-    if (board[row][col] == "X"){
-        return true;
-    }
-
-    else {
-        return false;
-    }
-}
-
 int main () {
     int row, col;
     int temp;
 
     int board[BOARD][BOARD] = {};
     PrintBoard();
-    createMines(board[BOARD][BOARD]);
+    createMines(board);
 
     while (1) {
         printf("Enter a row: ");
@@ -93,7 +92,7 @@ int main () {
         printf("Enter a column: ");
         scanf("%d", &col);
 
-        RedrawBoard(board);
+        ShowBoard(board);
     }
 
     return 0;
